d5542dec323349e6ae84a6f49ee7e15ef8123c84
Raghul Raj Manogeran <raghulraj172@gmail.com>
2023-07-11 00:43:05 -0700

    Create Solution.py

---
+++ Medium/1. Maximum Subarray/Solution.py
@@ -0,0 +1,14 @@
+def maxSubArray(a: List[int]) -> int:
+  maxSum = a[0]
+  cumSum = 0
+  for i in a:
+    if cumSum < 0:
+      cumSum = 0
+
+    cumSum += i
+    #Instead, we could do, max(maxSum, cumSum) but that's slow 
+    if maxSum < cumSum:
+      maxSum = cumSumm
+  return (maxSum)
+#Because of function call overhead and irrespective of the condition 
+#maxSum < cumSum being true or not, the function call will happen!!
